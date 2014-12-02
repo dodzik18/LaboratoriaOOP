7db1d7e5de7a9443d4dec817464364d306fde8e7
dodzik18 <dodzik18@gmail.com>
2014-12-02 14:18:10 +0100

    whole program is working now

--- Laboratorium2/Lab2/Source.cpp
+++ Laboratorium2/Lab2/Source.cpp
@@ -19,34 +19,39 @@ void wypisz(const pkt &p2)
 	std::cout << "(" << p2.x << "," << p2.y << ")" << std::endl;
 }
 
-void wypisz(prosta &pr1)
+void SprawdzProstopadlosc(prosta pr2, prosta pr3)
 {
-	std::cout << "Prosta przechodzi przez punkty: (" << pr1.x1 << "," << pr1.x2 << "),(" << pr1.y1 << "," << pr1.y2 << ")" << std::endl;
+	if (pr2.a == (-1 / pr3.a)) cout << "Proste sa prostopadle." << endl;
+	else cout << "Proste nie sa prostopadle." << endl;
 }
 
-
+void wypisz(prosta pr1)
+{
+	cout << "Prosta przechodzi przez punkty";
+	pr1._p1._wypisz(); pr1._p2._wypisz();
+	cout << "i ma rownianie y=";
+	cout << pr1.a << "x";
+	pr1.znak(pr1.b);
+	cout << pr1.b << endl;
+}
 
 int main()
 {
 	pkt p1, p2(2, 5), p3(3, 7), p4(p3);
 	prosta pr1, pr2(p2, p3), pr3(pr2);
 	p1.wypisz();
-	p2.wypisz();// wspolrzedne punktu (2,5)
+	p2.wypisz();
 	p3.wypisz();
 	p4.wypisz();
 	pr1.wypisz();
 	pr2.wypisz();
-	// prosta przechodzi przez punkty (2,5) (3,7) i ma równanie y = 2x + 1
 	pr3.wypisz();
-	//pr1.SprawdzRownoleglosc(pr2);
-	// tu ma byæ informacja czy proste s¹ równoleg³e 
-	//SprawdzProstopadlosc(pr2, pr3);
-	// tu ma byæ informacja czy proste s¹ prostopad³e
+	pr1.SprawdzRownoleglosc(pr2);
+	SprawdzProstopadlosc(pr2, pr3);
 	p1 = p2.suma(p3);
 	p1.wypisz();
 	p2 = roznica(p1, p3);
 	wypisz(p2);
 	wypisz(pr1);
-	// wypisanie informacji o punktach Np.: „Prosta y =ax+b przechodzi przez punkty...
 	return 0;
 }
--- Laboratorium2/Lab2/pkt.cpp
+++ Laboratorium2/Lab2/pkt.cpp
@@ -10,6 +10,22 @@ void pkt::wypisz()
 {
 	std::cout << "Wspolrzedne punktu: " << "(" << x << "," << y << ")" << std::endl;
 }
+void pkt::_wypisz()
+{
+	std::cout << " (" << x << "," << y << ") ";
+}
+
+void pkt::pobierz(float &_x, float &_y)
+{
+	_x = x;
+	_y = y;
+}
+
+void pkt::ustaw(float &_x, float &_y)
+{
+	x = _x;
+	y = _y;
+}
 
 pkt pkt::suma(const pkt &p3) //ok
 {
