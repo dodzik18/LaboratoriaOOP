98ca0b381b21797a8dfae17fc526facae894d942
dodzik18 <dodzik18@gmail.com>
2014-12-03 15:36:18 +0100

    added code

---
+++ Laboratorium3/Laboratorium3/LiczbaZespolona.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class LiczbaZespolona
+{
+public:
+	LiczbaZespolona(double x = 0., double y = 0.) : re(x), im(y) {};
+	LiczbaZespolona(const LiczbaZespolona &lz);
+
+	void pobierz(double &x, double &y);
+
+
+private:
+	double re, im;
+};
