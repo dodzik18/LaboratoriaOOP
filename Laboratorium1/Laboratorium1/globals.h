1d91fe8d145357797a8e37ca48f22547fedb2413
dodzik18 <dodzik18@gmail.com>
2014-12-21 18:31:23 +0100

    excluded Lab5 code

---
+++ Laboratorium1/Laboratorium1/globals.h
@@ -0,0 +1,9 @@
+#include "Date.h"
+#include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+void show(Person&);
+ostream & operator<<(ostream&, Person&);
+ostream & operator<<(ostream&, Date&);
