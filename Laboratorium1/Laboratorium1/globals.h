5a8de933e3ad6be7d3f48fd3dd066509d09ae838
dodzik18 <dodzik18@gmail.com>
2014-12-21 18:43:12 +0100

    removed unnecessary file

--- Laboratorium1/Laboratorium1/globals.h
+++
@@ -1,9 +0,0 @@
-#include "Date.h"
-#include "Person.h"
-#include <iostream>
-
-using namespace std;
-
-void show(Person&);
-ostream & operator<<(ostream&, Person&);
-ostream & operator<<(ostream&, Date&);
