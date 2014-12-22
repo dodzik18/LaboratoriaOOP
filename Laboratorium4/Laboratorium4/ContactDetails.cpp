52eb172de79ccbfbed850316139d3aad80872484
dodzik18 <dodzik18@gmail.com>
2014-12-22 23:20:35 +0100

    added ContactDetails copying constructor, along with appropriate getters.

--- Laboratorium4/Laboratorium4/ContactDetails.cpp
+++ Laboratorium4/Laboratorium4/ContactDetails.cpp
@@ -1,2 +1,7 @@
 #include "ContactDetails.h"
 
+ContactDetails::ContactDetails(const ContactDetails& Details)
+{
+	postalCode = Details.postalCode;
+	idCardNumber = Details.idCardNumber;
+}
--- Laboratorium4/Laboratorium4/ContactDetails.h
+++ Laboratorium4/Laboratorium4/ContactDetails.h
@@ -4,8 +4,10 @@ class ContactDetails
 public:
 	ContactDetails(int postalCode_ = 0, int idCardNumber_ = 0)
 		:postalCode(postalCode_), idCardNumber(idCardNumber_) {};
+	ContactDetails(const ContactDetails&);
 
-
+	int getPostalCode() { return postalCode; };
+	int getidCardNumber() { return idCardNumber; };
 
 private:
 	int postalCode, idCardNumber;
