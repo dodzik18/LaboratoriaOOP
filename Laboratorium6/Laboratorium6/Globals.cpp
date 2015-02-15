be8b5cbe59648914635ae9a80b1af8c2121598b8
dodzik18 <dodzik18@gmail.com>
2015-02-16 00:56:38 +0100

    Moved friend functions to separate file, and made them accordance to standards

--- Laboratorium6/Laboratorium6/Globals.cpp
+++ Laboratorium6/Laboratorium6/Globals.cpp
@@ -18,9 +18,27 @@ std::ostream& operator<<(std::ostream& out, Point3d& point_)
 
 std::ostream& operator<<(std::ostream& out, Vector& vector_)
 {
-	Point3d p1_, p2_;
-	p1_ = vector_.getP1();
-	p2_ = vector_.getP2();
-	out << "(" << p1_ << "," << p2_ << ") " << "<" << vector_.getD1() << "," << vector_.getD2() << "," << vector_.getD3() << ">" ;
+	vector_.showDattributes();
 	return out;
+}
+/* http://en.cppreference.com/w/cpp/language/operators -> Binary arithmetic operators */
+Point2d& operator+(Point2d& lPoint, const Point2d& rPoint)
+{
+	return lPoint += rPoint;
+}
+
+Point3d& operator+(Point3d& lPoint, const Point3d& rPoint)
+{
+	return lPoint += rPoint;
+}
+
+Vector& operator+(Vector& lVector, const Vector& rVector)
+{
+	return lVector += rVector;
+}
+
+float operator!(const Vector& vector_)
+{
+	/* sqrt(x*x+y*y+z*z) */
+	return  sqrt(vector_.getP1_X()*vector_.getP1_X() + vector_.getP1_Y()*vector_.getP1_Y() + vector_.getP1_Z()*vector_.getP1_Z());
 }
--- Laboratorium6/Laboratorium6/Globals.h
+++ Laboratorium6/Laboratorium6/Globals.h
@@ -3,3 +3,4 @@ class Vector;
 std::ostream& operator<<(std::ostream&, Point2d&);
 std::ostream& operator<<(std::ostream&, Point3d&);
 std::ostream& operator<<(std::ostream&, Vector&);
+float operator!(const Vector&);
