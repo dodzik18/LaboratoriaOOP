34d97dd6eef314a37c2d73d314ead3af4ec85af9
dodzik18 <dodzik18@gmail.com>
2015-03-06 12:36:17 +0100

    Code review #3

--- Laboratorium6/Laboratorium6/Point2d.cpp
+++ Laboratorium6/Laboratorium6/Point2d.cpp
@@ -1,48 +1,64 @@
 #include "Point2d.h"
 #include <iostream>
 
-Point2d::Point2d(double x_, double y_)
+Point2d::Point2d(double x, double y)
 {
 	xy = new double[2];
-	xy[0] = x_;
-	xy[1] = y_;
+	xy[0] = x;
+	xy[1] = y;
 }
-Point2d::Point2d(const Point2d& point_)
+Point2d::Point2d(const Point2d& point2D)
 {
 	xy = new double[2];
-	xy[0] = point_.xy[0];
-	xy[1] = point_.xy[1];
+	xy[0] = point2D.xy[0];
+	xy[1] = point2D.xy[1];
 }
+
 Point2d::~Point2d()
 {	
 	delete[] xy;
 }
 
-Point2d& Point2d::operator+=(const Point2d& point_)
+Point2d& Point2d::operator+=(const Point2d point2D)
 {
-	xy[0] += point_.getX();
-	xy[1] += point_.getY();
+	xy[0] += point2D.getX();
+	xy[1] += point2D.getY();
 	return *this;
 }
 
-Point2d& Point2d::operator=(const Point2d& point_)
+Point2d& Point2d::operator=(const Point2d point2D)
 {
-	xy[0] = point_.xy[0];
-	xy[1] = point_.xy[1];
+	xy[0] = point2D.xy[0];
+	xy[1] = point2D.xy[1];
 	return *this;
 }
 
-Point2d& Point2d::operator-=(const Point2d& point_)
+Point2d& Point2d::operator-=(const Point2d point2D)
 {
-	xy[0] -= point_.getX();
-	xy[1] -= point_.getY();
+	xy[0] -= point2D.getX();
+	xy[1] -= point2D.getY();
 	return *this;
 }
 
-void Point2d::swap(Point2d& point_) 
+Point2d Point2d::operator+(const Point2d point2D) const
 {
-	std::swap(xy[0], point_.xy[0]);
-	std::swap(xy[1], point_.xy[1]);
+	Point2d temp;
+	temp.xy[0] = xy[0] + point2D.xy[0];
+	temp.xy[1] = xy[1] + point2D.xy[1];
+	return temp;
+}
+
+Point2d Point2d::operator-(const Point2d point2D) const
+{
+	Point2d temp;
+	temp.xy[0] = xy[0] - point2D.xy[0];
+	temp.xy[1] = xy[1] - point2D.xy[1];
+	return temp;
+}
+
+void Point2d::swap(Point2d& point2D) 
+{
+	std::swap(xy, point2D.xy);
 }
 
 void Point2d::show()
@@ -70,8 +86,8 @@ void Point2d::show(int choice)
 	}
 }
 
-void Point2d::set(double x_, double y_)
+void Point2d::set(double x, double y)
 {
-	xy[0] = x_;
-	xy[1] = y_;
+	xy[0] = x;
+	xy[1] = y;
 }
--- Laboratorium6/Laboratorium6/Point2d.h
+++ Laboratorium6/Laboratorium6/Point2d.h
@@ -3,29 +3,30 @@
 class Point2d
 {
 public:	
-	Point2d() {};
-	explicit Point2d(double x_, double y_);
-	Point2d(const Point2d&);
+	Point2d() = default;
+	explicit Point2d(double x, double y);
+	Point2d(const Point2d& point2D);
 	virtual ~Point2d();
 
 
-	void swap(Point2d&) throw();
+	void swap(Point2d& point2D) throw();
 
 
 	virtual void show();
 	virtual void show(int choice);
 
 
-	Point2d& operator+=(const Point2d&);
-	Point2d& operator=(const Point2d&);
-	Point2d& operator-=(const Point2d&);
-	friend Point2d& operator+(const Point2d&, const Point2d&); 
+	Point2d& operator+=(const Point2d point2D);
+	Point2d& operator=(const Point2d point2D);
+	Point2d& operator-=(const Point2d point2D);
+	Point2d operator+(const Point2d point2D) const;
+	Point2d operator-(const Point2d point2D) const;
 
 
-	virtual double getX() const { return xy[0]; }
-	virtual double getY() const { return xy[1]; }
+	double getX() const { return xy[0]; }
+	double getY() const { return xy[1]; }
 
-	void set(double x_, double y_);
+	void set(double x, double y);
 
 protected:
 	double *xy;
